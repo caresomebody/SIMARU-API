c958b1b513bdfce5b1e9b44d925e841240ff1109
Ali Naufal Ammarullah <ali_naufal26@apps.ipb.ac.id>
2021-06-14 23:20:23 +0700

    fixing

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json())
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://simaru-api.herokuapp.com/');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');

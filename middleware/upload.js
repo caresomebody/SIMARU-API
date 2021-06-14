ddd84d597960ac26db45d067a7d14c6fd0358638
Ali Naufal Ammarullah <ali_naufal26@apps.ipb.ac.id>
2021-06-15 02:56:04 +0700

    upload

--- middleware/upload.js
+++ middleware/upload.js
@@ -6,6 +6,6 @@ var storage = multer.diskStorage({
         cb(null, 'uploads/')
     }, 
     filename: function(req, file, cb){
-        cb(null, Date.now() + ext)
+        cb(null, Date.now() + + "-" + file.originalname)
     }
 })

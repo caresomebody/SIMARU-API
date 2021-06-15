3651702be21342c64c3e8aa8a26f626d835a7ef9
Ali Naufal Ammarullah <ali_naufal26@apps.ipb.ac.id>
2021-06-16 01:37:49 +0700

    update

--- routes/api/pengajuan.js
+++ routes/api/pengajuan.js
@@ -8,7 +8,7 @@ const upload = require('../../middleware/upload')
 router.get('/', authenticate, PengajuanController.index)
 router.get('/:id', authenticate, PengajuanController.show)
 router.post('/store', authenticate, upload.single('dokumen'), PengajuanController.store)
-router.patch('/update/:id', authenticate, PengajuanController.update)
+router.put('/update/:id', authenticate, PengajuanController.update)
 router.delete('/delete/:id', authenticate, PengajuanController.destroy)
 
 module.exports = router
